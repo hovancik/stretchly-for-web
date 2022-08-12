67c14156dfd764a812f4a26f7963f7e60ba55f75
jan <jan@hovancik.net>
2022-08-12 04:02:42 -0400

    Version 0.0.2

--- svelte.config.js
+++ svelte.config.js
@@ -4,20 +4,7 @@ import adapter from '@sveltejs/adapter-auto'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-
-    vite: {
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: '@use "src/variables.scss" as *;'
-          }
-        }
-      }
-    }
+    adapter: adapter()
   },
 
   preprocess: [
